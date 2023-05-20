e520f9d06aef5c32ce4d4e5421d5615ea872b638
Skylar <skylarblessings5@gmail.com>
2023-05-20 12:59:52 +0300

    task

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self):
+        self.value = None
+
+    def is_sentence(self):
+        if self.value and self.value.endswith('.'):
+            return True
+        return False
+
+    def is_question(self):
+        if self.value and self.value.endswith('?'):
+            return True
+        return False
+
+    def is_exclamation(self):
+        if self.value and self.value.endswith('!'):
+            return True
+        return False
+
+    def count_sentences(self):
+        if self.value:
+            sentences = self.value.split('.')
+            sentences = [s for s in sentences if s]  # Remove empty strings
+            return len(sentences)
+        return 0
